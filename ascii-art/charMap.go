4abd8adfe6e04b09b76957a3805090e11f598d1e
aboudchar <nakazakinakazaki22@gmail.com>
2025-01-13 17:59:05 +0100

    mostly completed

---
+++ ascii-art/charMap.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	//"strings"
+)
+
+var charMap = make(map[rune]int)
+
+func charMapping() {
+	chars := []rune{
+		'!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		':', ';', '<', '=', '>', '?', '@',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'[', '\\', ']', '^', '_', '`',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'{', '|', '}', '~',
+	}
+
+	index := 12
+	for i := 0; i < len(chars); i++ {
+		charMap[chars[i]] = index
+		index += 9
+	}
+}
+
+func getCharAddress(text string) []int {
+	runes := []rune(text)
+	addr := make([]int, len(text))
+
+	for i, r := range runes {
+		addr[i] = charMap[r]
+	}
+	return addr
+}
+
+func getCharsAscii(banner string, lines []int) error {
+	content, err := os.Open(banner)
+	if err != nil {
+		return err
+	}
+	defer content.Close()
+
+	r := bufio.NewScanner(content)
+
+	var allLines []string
+
+	for r.Scan() {
+		allLines = append(allLines, r.Text())
+	}
+
+	for i := 0; i < 8; i++ {
+        for j := 0; j < len(lines); j++ {
+            start := lines[j] - 1
+            if start+i < len(allLines) {
+                fmt.Print(allLines[start+i])
+            }
+        }
+        fmt.Println()
+    }
+	return nil
+}
--- ascii-art/main.go
+++ ascii-art/main.go
@@ -11,41 +11,41 @@ import (
 func main() {
 	args := os.Args
 
-	var outputFilename string
-	//var textToDraw string
-	var bannerToUse string
+	var outputFilename, textToDraw, bannerToUse string
 
-	// Cheking for the missing required arguments
-	if len(args) < 2 {
-		log.Fatal("You forgot to give a name to the output file.")
-	} else if len(args) < 3 {
-		log.Fatal("You forgot to add the text to draw.")
-	} else if len(args) < 4 {
-		log.Fatal("You didn't specify the banner.")
+	if len(args) < 2 || len(args) < 3 || len(args) < 4 {
+		errorMessage()
 	}
 
-	// Validating the input file name
 	if strings.HasPrefix(args[1], "--output=") {
 		outputFilename = args[1]
-		r := regexp.MustCompile(`--output=(\w+.txt)`)
-		outputFilename = r.FindString(outputFilename)
-		outputFilename = r.ReplaceAllString(outputFilename, "$1")
+		r := regexp.MustCompile(`--output=(\w+.txt)$`)
+		outputFilename = r.ReplaceAllString(r.FindString(outputFilename), "$1")
 		if !(strings.HasSuffix(outputFilename, ".txt")) {
-			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER] \nEX: go run . --output=<fileName.txt> something standard")
-			os.Exit(1)
+			errorMessage()
 		}
 	} else {
 		log.Fatal("Invalid output file name")
 	}
 
-	//text to draw = args[2]
+	textToDraw = args[2]
 	bannerToUse = args[3]
 
-	// Checking if the banner file exists
-	_, error := os.Open(bannerToUse + ".txt")
-
+	file, error := os.Open(bannerToUse + ".txt")
 	if error != nil {
 		log.Fatal(error)
 	}
+	defer file.Close()
 
+	charMapping()
+	err := getCharsAscii(bannerToUse+".txt", getCharAddress(textToDraw))
+	if err != nil{
+		log.Fatal(err)
+	}
+}
+
+// Prints the formating instructions and Exits with code 1
+func errorMessage() {
+	fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER] \nEX: go run . --output=<fileName.txt> something standard")
+	os.Exit(1)
 }
